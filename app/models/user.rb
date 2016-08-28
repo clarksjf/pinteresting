7feab28e79c6d7156f3a4fe8cdda2bb316cc1454
clarksjf <sjf.clark@gmail.com>
2016-08-28 19:47:49 +0100

    Implementation Devise for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
+  validates :password, :presence => true,
+                     :on => :create,
+                     :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+#Password restrictions explanations:
+#First, we match for at least 10 characters:
+#(?=.{10,})
+#Next, we match for digits (numbers), using "\d":
+#(?=.*\d)
+#Then, we match a lowercase alphabet character:
+#(?=.*[a-z])
+#And then we match an uppercase alphabet character:
+#(?=.*[A-Z])
+#This code matches a special character.  Note how each special character needs to be escaped using the "\" character:
+#(?=.*[\@\#\$\%\^\&\+\=])
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
