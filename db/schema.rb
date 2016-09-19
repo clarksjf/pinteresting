76f788ed60a52159d2d71a3b2b47fe517772a89f
clarksjf <sjf.clark@gmail.com>
2016-09-19 08:03:17 +0100

    Day 22 problems with user2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160912114629) do
+ActiveRecord::Schema.define(version: 20160917203926) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
